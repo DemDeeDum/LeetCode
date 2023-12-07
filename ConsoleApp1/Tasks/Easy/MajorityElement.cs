2eab57c0488455d6f435f174bf106963e012d573
Denys Plekhanov <plekhanov1090@gmail.com>
2023-12-07 11:06:52 -0600

    Commit

---
+++ ConsoleApp1/Tasks/Easy/MajorityElement.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Tasks.Easy
+{
+    internal class MajorityElement
+    {
+        public class Solution
+        {
+            public int MajorityElement(int[] nums)
+            {
+                if (nums.Length == 1)
+                {
+                    return nums[0];
+                }
+
+                var halfOfLength = nums.Length / 2;
+                for (var i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == int.MinValue)
+                    {
+                        continue;
+                    }
+
+                    var count = 1;
+                    for(var j = i + 1; j < nums.Length; j++)
+                    {
+                        if (nums[j] == nums[i])
+                        {
+                            count++;
+                            if (count > halfOfLength)
+                            {
+                                return nums[i];
+                            }
+
+                            nums[j] = int.MinValue;
+                        }
+                    }
+                }
+
+                return 0;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Tasks/Medium/RemoveDuplicatesFromSortedTwo.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Tasks.Medium
+{
+    internal class RemoveDuplicatesFromSortedTwo
+    {
+        public class Solution
+        {
+            public int RemoveDuplicates(int[] nums)
+            {
+                var numberOfDuplicates = 0;
+                for (var index = 0; index < nums.Length - numberOfDuplicates; index++)
+                {
+                    var found = false;
+                    for (var i = index + 1; i < nums.Length - numberOfDuplicates; i++)
+                    {
+                        if (nums[i] == nums[index])
+                        {
+                            if (!found)
+                            {
+                                found = true;
+
+                                continue;
+                            }
+
+                            MakeAShift(nums, i);
+                            numberOfDuplicates++;
+                            i--;
+                        }
+                    }
+                }
+
+                return nums.Length - numberOfDuplicates;
+            }
+
+            public void MakeAShift(int[] nums, int indexToShiftFrom)
+            {
+                var valueToPlaceToEnd = nums[indexToShiftFrom];
+                for(var i = indexToShiftFrom; i < nums.Length - 1; i++)
+                {
+                    nums[i] = nums[i + 1];
+                }
+
+                nums[nums.Length - 1] = valueToPlaceToEnd;
+            }
+        }
+    }
+}
