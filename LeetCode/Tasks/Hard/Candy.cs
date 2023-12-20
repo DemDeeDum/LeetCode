aab5f27003afc787931a5c0ccb8cd9c25d98ab64
Denys Plekhanov <plekhanov1090@gmail.com>
2023-12-20 14:20:04 -0600

    Commit

---
+++ LeetCode/Tasks/Hard/Candy.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode.Tasks.Hard
+{
+    internal class Candy
+    {
+        public class Solution
+        {
+            public int Candy(int[] ratings)
+            {
+                var lastMax = 0;
+                var lastMaxIndex = 0;
+                var prevExtraCandy = 0;
+                var total = ratings.Length;
+                for (var i = 0; i < ratings.Length; i++)
+                {
+                    var currentExtraCandy = 0;
+                    if (i > 0 && ratings[i] > ratings[i - 1])
+                    {
+                        currentExtraCandy += prevExtraCandy + 1;
+                        var j = i;
+                        while (j > 0 && ratings[j] < ratings[j - 1])
+                        {
+                            total++;
+                            j--;
+                        }
+
+                        lastMax = currentExtraCandy;
+                        lastMaxIndex = i;
+                    }
+                    
+                    if (i < ratings.Length - 1 && ratings[i] > ratings[i + 1] && currentExtraCandy == 0)
+                    {
+                        currentExtraCandy += 1;
+                        var j = i;
+                        while(j > 0 && ratings[j] < ratings[j - 1] && currentExtraCandy >= prevExtraCandy)
+                        {
+                            if (j - 1 == lastMaxIndex)
+                            {
+                                if (i - j + currentExtraCandy < lastMax)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    lastMax++;
+                                }
+                            }
+
+                            total++;
+                            j--;
+                        }
+                    }
+
+                    total += currentExtraCandy;
+                    prevExtraCandy = currentExtraCandy;
+                }
+
+                return total;
+            }
+        }
+    }
+}
---
+++ LeetCode/Tasks/Medium/GasStation.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Tasks.Medium
+{
+    internal class GasStation
+    {
+        public class Solution
+        {
+            public int CanCompleteCircuit(int[] gas, int[] cost)
+            {
+                var length = gas.Length;
+                for (var i = 0; i < length; i++)
+                {
+                    if (i == 0 || gas[i - 1] - cost[i - 1] < gas[i] - cost[i])
+                    {
+                        var gasAmount = 0;
+                        var j = i;
+                        while (true)
+                        {
+                            gasAmount += gas[j] - cost[j];
+                            if (gasAmount < 0)
+                            {
+                                break;
+                            }
+
+                            if (++j >= gas.Length)
+                            {
+                                j = 0;
+                            }
+
+                            if (j == i)
+                            {
+                                return i;
+                            }
+                        }
+                    }
+                }
+
+                return -1;
+            }
+        }
+    }
+}
