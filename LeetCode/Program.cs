7f3f058f0e7cf26d142a2bb2b76ab5d138dca82e
Denys Plekhanov <plekhanov1090@gmail.com>
2023-12-22 20:15:16 -0600

    Commit

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,11 +8,9 @@ class Program
     /// </summary>
     public static void Main()
     {
-        var a = new Candy.Solution();
-        var c = a.Candy(new[] { 1, 3, 2, 2, 1 }); // 7
-        //var c = a.Candy(new[] { 1, 2, 87, 87, 87, 2, 1 }); // 13
-        //var c = a.Candy(new[] { 1, 0, 2 }); // 5
-        //var c = a.Candy(new[] { 1, 2, 3, 1, 0 }); // 9
-        //var c = a.Candy(new[] { 1, 2, 3, 5, 4, 3, 2, 1 }); // 21
+        var a = new TrappingRainWater.Solution();
+        //var c = a.Trap(new[] { 4, 0, 3, 0, 2, 0, 1 });
+        var c = a.Trap(new[] { 4, 2, 0, 3, 2, 5 });
+        //var c = a.Trap(new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 });
     }
 }
---
+++ LeetCode/Tasks/Hard/TrappingRainWater.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode.Tasks.Hard
+{
+    internal class TrappingRainWater
+    {
+        public class Solution
+        {
+            public int Trap(int[] height)
+            {
+                var totalWater = 0;
+                var index = 0;
+                var valueIndex = -1;
+                var trunksSpace = 0;
+                var temporary = 0;
+
+                do
+                {
+                    if (height[index] > 0)
+                    {
+                        if (valueIndex == -1)
+                        {
+                            valueIndex = index;
+                        }
+                        else if (height[index] < height[valueIndex])
+                        {
+                            //if (height[prevViceMaxIndex] < height[index])
+                            //{
+                            //    temporary -= (height[valueIndex] - height[index]) * (height.Length - valueIndex);
+                            //}
+
+                            trunksSpace += height[index];
+                        }
+                        else if (height[index] >= height[valueIndex])
+                        {
+                            totalWater += height[valueIndex] * (index - valueIndex - 1) - trunksSpace;
+                            valueIndex = index;
+                            //temporary = height[valueIndex] * (height.Length - index - 1);
+                        }
+                    }
+                    else
+                    {
+                        temporary += height[valueIndex];
+                    }
+                }
+                while (++index < height.Length);
+
+                return totalWater;
+            }
+        }
+    }
+}
