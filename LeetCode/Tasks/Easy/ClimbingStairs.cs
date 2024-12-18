a28a3d8fb490418604c265b4b9665b4741ef0b02
Dmytro Lozhka <d1m.dee.dum@gmail.com>
2024-12-17 21:45:16 -0600

    COmmit

---
+++ LeetCode/Tasks/Easy/ClimbingStairs.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LeetCode.Tasks.Easy
+{
+    internal class ClimbingStairs
+    {
+        public class Solution
+        {
+            public int ClimbStairs(int n)
+            {
+                var a = 1;
+                var b = 1;
+
+                for (var i = 1; i < n; i++)
+                {
+                    b = a + b;
+                    a = b - a;
+                }
+
+                return b;
+            }
+        }
+    }
+}
---
+++ LeetCode/Tasks/Medium/LRUCache.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.Tasks.Medium
+{
+    public class LRUCache
+    {
+        private int maxCapacity;
+        private LinkedList<DataItem> keysUsage;
+        private Dictionary<int, LinkedListNode<DataItem>> storedData;
+
+        public LRUCache(int capacity)
+        {
+            keysUsage = new LinkedList<DataItem>();
+            storedData = new Dictionary<int, LinkedListNode<DataItem>>(capacity);
+            maxCapacity = capacity;
+        }
+
+        public int Get(int key)
+        {
+            if (storedData.ContainsKey(key))
+            {
+                var node = storedData[key];
+                keysUsage.Remove(node);
+                storedData[key] = keysUsage.AddFirst(node.Value);
+
+                return node.Value.Value;
+            }
+
+            return -1;
+        }
+
+        public void Put(int key, int value)
+        {
+            if (storedData.ContainsKey(key))
+            {
+                keysUsage.Remove(storedData[key]);
+            }
+            else if (maxCapacity <= storedData.Count)
+            {
+                var oldest = keysUsage.Last!.Value;
+                keysUsage.RemoveLast();
+                storedData.Remove(oldest.Key);
+            }
+
+            storedData[key] = keysUsage.AddFirst(new DataItem { Key = key, Value = value });
+        }
+
+        struct DataItem
+        {
+            public int Key;
+
+            public int Value;
+        }
+    }
+}
